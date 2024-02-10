468c27f2268fcae2ea8cbd8830e658adc3fb1b66
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-10 20:45:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-//your code here
+window.onload = function() {
+  let divs = document.querySelectorAll('.image');
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].setAttribute('id', 'div' + (i + 1));
+    divs[i].addEventListener("dragstart", handleDragStart, false);
+    divs[i].addEventListener("dragover", handleDragOver, false);
+    divs[i].addEventListener("drop", handleDrop, false);
+  }
+}
+
+let dragSrcEl = null;
+
+function handleDragStart(e) {
+  
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = 'move';
+}
+
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
+
+function handleDrop(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation();
+  }
+  if (dragSrcEl != this) {
+    let parent = document.querySelector('#parent');
+    let srcIndex = Array.prototype.indexOf.call(parent.children, dragSrcEl);
+    let targetIndex = Array.prototype.indexOf.call(parent.children, this);
+    if (srcIndex < targetIndex) {
+      parent.insertBefore(dragSrcEl, this.nextSibling);
+      parent.insertBefore(this, parent.children[srcIndex]);
+    } else {
+      parent.insertBefore(this, dragSrcEl);
+      parent.insertBefore(dragSrcEl, parent.children[targetIndex]);
+    }
+  }
+  return false;
+}
